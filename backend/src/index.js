30de23d1b5798851a22d1971fe2d57d6aca00482
czhcheng27 <czhcheng27@gmail.com>
2025-06-04 22:03:43 -0400

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,6 +17,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
