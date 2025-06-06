f3d6e69b37d6e63438b963ab70a0ac4f6273f39d
czhcheng27 <czhcheng27@gmail.com>
2025-06-06 11:21:12 -0400

    add theme switch function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,9 +9,11 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -25,7 +27,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
