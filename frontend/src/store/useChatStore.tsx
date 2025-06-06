68fd7f7b330d6876155cca2d2efb7bd575335013
czhcheng27 <czhcheng27@gmail.com>
2025-06-06 15:19:42 -0400

    update message type

--- frontend/src/store/useChatStore.tsx
+++ frontend/src/store/useChatStore.tsx
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 import api from "../lib/apiClient";
-import type { ChatState } from "../types/chat";
+import type { ChatState, Message } from "../types/chat";
 import type { AuthUser } from "../types/auth";
 
-export const useChatStore = create<ChatState>((set) => ({
+export const useChatStore = create<ChatState>((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -17,11 +17,20 @@ export const useChatStore = create<ChatState>((set) => ({
     set({ isUsersLoading: false });
   },
 
-  getMessages: async (userId: string) => {
+  getMessages: async (userId) => {
     set({ isMessagesLoading: true });
-    const res = await api.get<object[]>(`/messages/${userId}`);
+    const res = await api.get<Message[]>(`/messages/${userId}`);
     res && set({ messages: res });
     set({ isMessagesLoading: false });
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    const res = await api.post<Message>(
+      `/messages/send/${selectedUser?._id}`,
+      messageData
+    );
+    if (res) set({ messages: [...messages, res] });
   },
 
   setSelectedUser: (selectedUser) => set({ selectedUser }),
--- frontend/src/types/chat.ts
+++ frontend/src/types/chat.ts
@@ -1,12 +1,23 @@
 import type { AuthUser } from "./auth";
 
 export interface ChatState {
-  messages: Array<object>;
+  messages: Array<Message>;
   users: Array<AuthUser>;
   selectedUser: AuthUser | null;
   isUsersLoading: boolean;
   isMessagesLoading: boolean;
   getUsers: () => Promise<void>;
   getMessages: (data: string) => Promise<void>;
+  sendMessage: (data: SendMsg) => Promise<void>;
   setSelectedUser: (data: AuthUser | null) => void;
+}
+interface SendMsg {
+  text: string;
+  image?: string | null;
+}
+export interface Message extends SendMsg {
+  senderId: string;
+  receiverId: string;
+  _id: string;
+  createdAt: string;
 }
