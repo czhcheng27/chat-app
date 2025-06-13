ce5418f9466b4487f289aa463b388a09dcf9c406
chengzihang <czhcheng27@gmail.com>
2025-06-14 01:56:53 +0800

    backend index revert

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { fileURLToPath } from "url";
+
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
@@ -14,10 +14,8 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const staticPath = path.join(__dirname, "../frontend/dist");
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 const allowedOrigins = [
   "http://localhost:5173", // Vite
@@ -43,11 +41,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/ai", aiRoutes);
 
-app.use(express.static(staticPath));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(staticPath, "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
