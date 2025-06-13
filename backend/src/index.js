a9cdec0fb10e96ee0b94ba5f8a320a1c70412aa2
chengzihang <czhcheng27@gmail.com>
2025-06-14 02:09:45 +0800

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import { fileURLToPath } from 'url';
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
@@ -15,7 +15,8 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
   "http://localhost:5173", // Vite
