52da9da287aed86595d6e7cd98392b67780ca6f7
czhcheng27 <czhcheng27@gmail.com>
2025-09-27 21:26:26 -0400

    新增允许访问域名

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
   "https://czhcheng27.github.io",
   "https://chat-app-244z.onrender.com",
   "https://chat-app-tan-chi.vercel.app",
+  "https://projects.czhcheng27.workers.dev",
 ];
 
 app.use(express.json({ limit: "5mb" }));
