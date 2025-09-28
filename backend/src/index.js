3c6a689f8a05605e60298c1358ca6c50fb1d258e
czhcheng27 <czhcheng27@gmail.com>
2025-09-27 20:17:41 -0400

    准备VITE_API_BASE_URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,11 @@ app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://chat-app-tan-chi.vercel.app",
+    ],
     credentials: true,
   })
 );
